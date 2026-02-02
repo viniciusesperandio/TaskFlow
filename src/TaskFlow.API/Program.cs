0adfb4195809cac4c76d046d8f9f338cd7c621e3
vinicius.esperandio <vinicius.esperandio@hotmail.com>
2026-02-01 23:39:35 -0300

    feat: - Setting env files - Added dockerfiles e configs - Added Prometheus and grafana yml files

--- src/TaskFlow.API/Program.cs
+++ src/TaskFlow.API/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -32,6 +35,9 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
 
